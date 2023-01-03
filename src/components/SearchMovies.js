06683aa9afa0075cb9115c5b0fe6d0263270c361
Jess <jessjelardo@gmail.com>
2023-01-03 19:04:51 +0800

    integrated api to the form and created state query and movie state

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 const SearchMovies = () => {
+    
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (e) => {
         e.preventDefault();
 
-        const query = "The Simpsons"
-
-        const url = `https://api.themoviedb.org/3/movie/550?api_key=c5f7422cdd758485fceab81926ac45ba&language=en-US&query=${query}&page=1&include_adult=false`;
+        // const url = `https://api.themoviedb.org/3/movie/550?api_key=c5f7422cdd758485fceab81926ac45ba&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=c5f7422cdd758485fceab81926ac45ba&query=${query}`
 
         try {
             const res = await fetch(url);
             const data = await res.json();
-            console.log(data)
+            console.log(data.results)
+            setMovies(data.results)
         }
         catch(e){
             console.error(e);
@@ -24,7 +27,13 @@ const SearchMovies = () => {
     return (
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" name="query" type='text' placeholder="search here..."/>
+            <input 
+            className="input" 
+            name="query" 
+            type='text' 
+            placeholder="search here..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}/>
             <button className="button" type="submit">Search</button>
         </form>
     )
